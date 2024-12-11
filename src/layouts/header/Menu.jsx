91fbd4d256dfb2ab41857f33f504b213959f865b
Max Gaillard <maxgaillar@gmail.com>
2024-12-11 10:55:37 -0500

    try fix generating tailwind utility classes dynamically

--- src/layouts/header/Menu.jsx
+++ src/layouts/header/Menu.jsx
@@ -6,11 +6,18 @@ const Menu = () => {
   const { menuRef } = useNavContext();
   const { publicMenu } = menu;
   return (
-    <ul id="menu" className="menu w-full bg-slate-100" ref={menuRef}>
+    <ul
+      id="menu"
+      className="menu w-full bg-slate-100 border-t border-slate-200"
+      ref={menuRef}
+    >
       {publicMenu.map((option, i) => {
         return (
           <li key={i}>
-            <NavLink className={option.style} to={option.route}>
+            <NavLink
+              className="block text-large border-b p-3 border-slate-200"
+              to={option.route}
+            >
               {option.label}
             </NavLink>
           </li>
