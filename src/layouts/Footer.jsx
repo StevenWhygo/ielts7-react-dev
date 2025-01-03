2a5df4d5878cfda1ac54cd37b3c96ef886ba579a
Max Gaillard <maxgaillar@gmail.com>
2025-01-03 15:20:26 -0500

    add gap on page sections

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex items-center w-full min-w-148 min-h-12 bg-slate-800 text-slate-100">
+    <footer className="flex items-center w-full min-w-148 min-h-12 mt-4 bg-slate-800 text-slate-100">
       <div className="text-center w-full text-xs">
         Copyright &copy;2024 IELTS7 Taipei All rights reserved
       </div>
