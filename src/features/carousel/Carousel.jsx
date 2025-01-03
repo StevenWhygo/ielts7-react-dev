8fead03caf73fbcb9a77642bb6d5d4f6ff4260d7
Max Gaillard <maxgaillar@gmail.com>
2025-01-03 09:14:57 -0500

    swithced slides images and card test

--- src/features/carousel/Carousel.jsx
+++ src/features/carousel/Carousel.jsx
@@ -1,48 +1,26 @@
-import { useEffect, useState, useRef } from 'react';
+import { useLayoutEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import Card from '../../components/Card';
-import useSlider from '../../hooks/useSlider';
 import { IconContext } from 'react-icons';
 import { TfiAngleLeft } from 'react-icons/tfi';
 import { TfiAngleRight } from 'react-icons/tfi';
+import { FaRegCircle } from 'react-icons/fa6';
+import { FaRegCircleDot } from 'react-icons/fa6';
 import { GoDotFill } from 'react-icons/go';
 
 const Carousel = ({ slides, cards }) => {
+  const [slideIndex, setSlideIndex] = useState(0);
   const carouselRef = useRef(null);
   const leftArrowRef = useRef(null);
   const rightArrowRef = useRef(null);
-
-  const {
-    handleSetSlider,
-    handleNextSlide,
-    handleIndex,
-    handleOffset,
-    handleCurrentSlides,
-    index,
-    offset,
-    currentSlides,
-    isSet,
-  } = useSlider();
-
-  useEffect(() => {
-    carouselRef.current.style.left = `${-100}%`;
-    handleSetSlider({
-      index: 0,
-      offset: -100,
-      currentSlides: [slides[slides.length - 1], slides[0], slides[1]],
-      slides: slides,
-      slider: carouselRef.current,
-    });
-  }, []);
 
   const arrows = [
     {
       attributes: {
         id: 'left',
-        className:
-          'block absolute inset-y-0 transition-opacity pr-12 hover:opacity-60',
-        ariaLabel: `View Image ${index}`,
+        className: 'arrow transition-opacity pr-3/12 hover:opacity-60',
+        ariaLabel: `View Image ${slideIndex}`,
       },
       ref: leftArrowRef,
       icon: <TfiAngleLeft />,
@@ -50,9 +28,8 @@ const Carousel = ({ slides, cards }) => {
     {
       attributes: {
         id: 'right',
-        className:
-          'block absolute inset-y-0 right-0 pl-12 transition-opacity hover:opacity-60',
-        ariaLabel: `View Image ${index}`,
+        className: 'arrow right-0 pl-3/12 transition-opacity hover:opacity-60',
+        ariaLabel: `View Image ${slideIndex}`,
       },
       ref: rightArrowRef,
       icon: <TfiAngleRight />,
@@ -61,84 +38,72 @@ const Carousel = ({ slides, cards }) => {
 
   const nextSlide = (e) => {
     if (e.target.id === 'right') {
-      if (index < slides.length - 1) {
-        handleNextSlide({
-          index: index + 1,
-          offset: -100,
-          isSet: true,
-        });
-      } else {
-        handleNextSlide({ index: 0, offset: -100, isSet: true });
+      if (slideIndex === slides.length - 1) {
+        return setSlideIndex(0);
       }
+      // right arrow
+      setSlideIndex((prev) => (prev < 2 ? prev + 1 : prev));
     } else if (e.target.id === 'left') {
-      if (index > 0) {
-        handleNextSlide({
-          index: index - 1,
-          offset: 100,
-          isSet: true,
-        });
-      } else {
-        handleNextSlide({
-          index: slides.length - 1,
-          offset: 0,
-          isSet: true,
-        });
+      if (slideIndex === 0) {
+        return setSlideIndex(slides.length - 1);
       }
+      setSlideIndex((prev) => (prev > 0 ? prev - 1 : 0));
     } else {
-      handleNextSlide({
-        index: parseInt(e.target.id),
-        offset: -100,
-        isSet: true,
-      });
+      // slide indicators
+      setSlideIndex(parseInt(e.target.id));
     }
   };
+
+  useLayoutEffect(() => {
+    // switch slide: update translate property value on slide index change
+    carouselRef.current.style.left = `${-100 * slideIndex}%`;
+  }, [slideIndex]);
 
   return (
     <section className="carousel">
       <div className="relative overflow-hidden">
         <div className="slider" ref={carouselRef}>
-          {currentSlides &&
-            currentSlides.map((slide, i) => {
-              return (
-                <div key={i} className="relative">
-                  <picture className="relative flex-100">
-                    {slide.sources.map((source, i) => {
-                      return (
-                        <source
-                          key={i}
-                          srcSet={source.srcset}
-                          media={source.media}
-                        />
-                      );
-                    })}
-                    <img
-                      className="slide-image"
-                      src={slide.img.src}
-                      alt={slide.img.alt}
-                    />
-                  </picture>
-                  <>
-                    <div className="absolute top-0 p-4 m-4 w-11/12 min-h-44  bg-slate-50 opacity-40"></div>
-                    <div className="absolute flex flex-col top-0 p-4 m-4 w-11/12 min-h-44">
-                      <header className="font-bold text-xl pb-2 mb-2 border-b border-slate-950">
-                        {slide.card.header}
-                      </header>
-                      {/* <p className="text-sm mb-2">{slide.card.body}</p> */}
-                      <div className="mt-auto text-right">
-                        <Link
-                          className="inline-block w-32 py-2 text-sm text-center border-blue bg-sky-600 text-slate-50 font-bold hover:opacity-80"
-                          to={{
-                            pathname: `${slide.card.link.to}`,
-                          }}
-                        >
-                          {slide.card.link.value}
-                        </Link>
-                      </div>
+          {slides.map((slide, i) => {
+            return (
+              <div key={i} className="relative">
+                <picture className="relative flex-100">
+                  {slide.sources.map((source, i) => {
+                    return (
+                      <source
+                        key={i}
+                        srcSet={source.srcset}
+                        media={source.media}
+                      />
+                    );
+                  })}
+                  <img
+                    src={slide.img.src}
+                    className="slide-image"
+                    alt={slide.img.alt}
+                  />
+                </picture>
+                <>
+                  <div className="absolute top-0 p-4 m-4 w-11/12 min-h-44  bg-slate-50 opacity-40"></div>
+                  <div className="absolute flex flex-col top-0 p-4 m-4 w-11/12 min-h-44">
+                    <header className="font-bold text-xl pb-2 mb-2 border-b border-slate-950">
+                      {slide.card.header}
+                    </header>
+                    {/* <p className="text-sm mb-2">{slide.card.body}</p> */}
+                    <div className="mt-auto text-right">
+                      <Link
+                        className="inline-block w-32 py-2 text-sm text-center border-blue bg-sky-600 text-slate-50 font-bold hover:opacity-80"
+                        to={{
+                          pathname: `${slide.card.link.to}`,
+                        }}
+                      >
+                        {slide.card.link.value}
+                      </Link>
                     </div>
-                  </>
-                </div>
-              );
-            })}
+                  </div>
+                </>
+              </div>
+            );
+          })}
         </div>
 
         <>
@@ -150,16 +115,14 @@ const Carousel = ({ slides, cards }) => {
                 handler={nextSlide}
                 ref={arrow.ref}
               >
-                <div className="pointer-events-none">
-                  <IconContext.Provider
-                    value={{
-                      color: '#1e293b',
-                      size: '3rem',
-                    }}
-                  >
-                    {arrow.icon}
-                  </IconContext.Provider>
-                </div>
+                <IconContext.Provider
+                  value={{
+                    color: '#1e293b',
+                    size: '3rem',
+                  }}
+                >
+                  <div>{arrow.icon}</div>
+                </IconContext.Provider>
               </Button>
             );
           })}
@@ -176,27 +139,26 @@ const Carousel = ({ slides, cards }) => {
                 }}
                 handler={nextSlide}
               >
-                <span className="pointer-events-none">
-                  {index === key ? (
-                    <IconContext.Provider
-                      value={{
-                        color: '#0080c8',
-                        size: '1.2rem',
-                      }}
-                    >
-                      <GoDotFill />
-                    </IconContext.Provider>
-                  ) : (
-                    <IconContext.Provider
-                      value={{
-                        color: '#e2e8f0',
-                        size: '1.2rem',
-                      }}
-                    >
-                      <GoDotFill />
-                    </IconContext.Provider>
-                  )}
-                </span>
+                {/* {slideIndex === key ? <FaRegCircleDot /> : <FaRegCircle />} */}
+                {slideIndex === key ? (
+                  <IconContext.Provider
+                    value={{
+                      color: '#0080c8',
+                      size: '1.2rem',
+                    }}
+                  >
+                    <GoDotFill />
+                  </IconContext.Provider>
+                ) : (
+                  <IconContext.Provider
+                    value={{
+                      color: '#e2e8f0',
+                      size: '1.2rem',
+                    }}
+                  >
+                    <GoDotFill />
+                  </IconContext.Provider>
+                )}
               </Button>
             );
           })}
