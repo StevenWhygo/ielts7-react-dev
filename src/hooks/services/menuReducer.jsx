82c6977b8382371083462123d9ffc9e9396c9380
Max Gaillard <maxgaillar@gmail.com>
2025-02-28 11:57:29 -0500

    added transition and delay to submenus

--- src/hooks/services/menuReducer.jsx
+++ src/hooks/services/menuReducer.jsx
@@ -1,39 +1,41 @@
 export const menuReducer = (state, action) => {
     switch (action.type) {
-        case 'display':
+      case 'initialize':
         return {
-            ...state,
-            isDisplay: action.payload
-        }
-        case 'reset':
+          ...state,
+          currentIndex: action.payload.index,
+          displaySubmenu: action.payload.display,
+        };
+      case 'update':
         return {
-            ...action.payload,
-        }
-        case 'index':
-            return {
-                ...state,
-                currentIndex: action.payload
-            }
-    //   case 'open':
-    //     return {
-    //       ...state,
-    //       ...action.payload,
-    //     };
-    //   case 'menu':
-    //     return {
-    //     ...state,
-    //       currentMenu: action.payload
-    //     };
-    //   case 'button':
-    //     return {
-    //       ...state,
-    //       currentBtn: action.payload,
-    //     }
-    //   case 'close':
-    //     return {
-    //       ...state,
-    //       ...action.payload
-    //     }
+          ...state,
+          currentIndex: action.payload.index,
+          delay: action.payload.delay,
+        };
+      case 'reset':
+        return {
+          ...action.payload,
+        };
+      //   case 'open':
+      //     return {
+      //       ...state,
+      //       ...action.payload,
+      //     };
+      //   case 'menu':
+      //     return {
+      //     ...state,
+      //       currentMenu: action.payload
+      //     };
+      //   case 'button':
+      //     return {
+      //       ...state,
+      //       currentBtn: action.payload,
+      //     }
+      //   case 'close':
+      //     return {
+      //       ...state,
+      //       ...action.payload
+      //     }
       default:
         return null;
     }
