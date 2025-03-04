93bc2faeac89543590aab87c8a00c1fd3afa406c
Max Gaillard <maxgaillar@gmail.com>
2025-03-04 17:28:22 -0500

    styled footer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import useCard from '../hooks/useCard';
 
 const Card = (props) => {
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import useIcons from '../hooks/useIcons';
 
 const Section = ({ data }) => {
@@ -12,12 +12,20 @@ const Section = ({ data }) => {
     switch (name) {
       case 'social':
         return (
-          <ul>
+          <ul className="pl-2 flex gap-8">
             {items.map((item, i) => {
               return (
-                <li className="inline">
-                  <a href={item.url} title={item.icon} target="_blank">
-                    {fetchIcon(item.icon.toLowerCase())}
+                <li key={i} className="inline-block">
+                  <a
+                    className=""
+                    href={item.url}
+                    title={item.icon}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    {fetchIcon(item.icon.toLowerCase(), {
+                      size: '2.5rem',
+                    })}
                   </a>
                 </li>
               );
@@ -26,13 +34,18 @@ const Section = ({ data }) => {
         );
       case 'information':
         return (
-          <ul>
+          <ul className="pl-2">
             {items.map((item, i) => {
               return (
-                <li>
-                  <a href={item.url} title={item.text}>
+                <li key={i} className="flex items-center">
+                  <Link
+                    className="flex-1 h-full min-h-8"
+                    to={{
+                      pathname: item.url,
+                    }}
+                  >
                     {item.text}
-                  </a>
+                  </Link>
                 </li>
               );
             })}
@@ -40,16 +53,23 @@ const Section = ({ data }) => {
         );
       case 'connect':
         return (
-          <div>
-            <p>{text}</p>
+          <div className="px-2">
+            <p className="mb-4">{text}</p>
             <form>
               <div className="form-element">
-                <label htmlFor="email">Email*</label>
-                <input type="email" name="email" id="email" />
-              </div>
-              <div className="form-element">
-                <label htmlFor="email">Email*</label>
-                <textarea name="message" id="message" />
+                <label className="block mb-2" htmlFor="email">
+                  Email
+                </label>
+
+                <input
+                  className="inline-block p-2 mr-4 rounded-sm w-2/3 text-stone-50"
+                  type="email"
+                  name="email"
+                  id="email"
+                />
+                <button className="border rounded-sm px-4 py-2 " type="submit">
+                  Submit
+                </button>
               </div>
             </form>
           </div>
@@ -59,11 +79,9 @@ const Section = ({ data }) => {
     }
   };
 
-  useEffect(() => {}, []);
-
   return (
-    <section>
-      <h2>{title}</h2>
+    <section className="mb-8">
+      <h2 className="border-b pb-2 mb-6 text-lg font-semibold">{title}</h2>
       {sections(rest)}
     </section>
   );
